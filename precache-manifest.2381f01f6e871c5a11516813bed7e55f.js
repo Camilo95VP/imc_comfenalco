6fcc527b566207e67494e9308e6f5e4d996ce4b1
JJuanC <camiloflstudio@gmail.com>
2020-08-27 13:06:18 -0500

    Updates

---
+++ precache-manifest.2381f01f6e871c5a11516813bed7e55f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7d7f02a73fb09d3ff1db927d25958f",
+    "url": "/Camilo95VP/imc_comfenalco/index.html"
+  },
+  {
+    "revision": "7be36ec4993347c197be",
+    "url": "/Camilo95VP/imc_comfenalco/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e64b266f909918925f3a",
+    "url": "/Camilo95VP/imc_comfenalco/static/js/2.1a295d35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Camilo95VP/imc_comfenalco/static/js/2.1a295d35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be36ec4993347c197be",
+    "url": "/Camilo95VP/imc_comfenalco/static/js/main.d0503d20.chunk.js"
+  },
+  {
+    "revision": "199c67c27630f7e410fa",
+    "url": "/Camilo95VP/imc_comfenalco/static/js/runtime-main.ba4d83f9.js"
+  }
+]);
